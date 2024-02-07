5a46a4b048bd48cc1a97dbc7bce3b3dcf12b6c04
kang <2323748400@qq.com>
2024-02-07 10:04:20 +0800

    实现展示日程功能

--- main.js
+++ main.js
@@ -46,22 +46,17 @@ function render(time){
         
         days.innerHTML = ''   //清空日期区域
         let liCount = 0
+        const fragment  = document.createDocumentFragment()
         //根据月初星期几，在前铺垫几天
         for(let i =1 ; i< 月初星期几 ; i++){
             const li = document.createElement('li')
             const preDay = new Date(月初 - 86400*1000*i)
             li.textContent = preDay.getDate()
             li.classList.add('calendar-days-noThisMonth')
-            days.prepend(li)
-            li.onclick = ()=>{
-                if(selectedLi){
-                    selectedLi.classList.remove('calendar-days-selected')
-                }
-                li.classList.add('calendar-days-selected')
-                selectedLi = li
-            }
+            fragment.prepend(li)
             liCount += 1
         }
+
         //初始化 本月日期
         for(let i =1 ; i<= 这个月多少天 ; i++){
             const li = document.createElement('li')
@@ -69,16 +64,48 @@ function render(time){
             if(i === now.getDate() && month === now.getMonth()+1 && year === now.getFullYear()){
                 li.classList.add('calendar-days-today')
             }
-            li.onclick = ()=>{
-                if(selectedLi){
-                    selectedLi.classList.remove('calendar-days-selected')
-                }
-                li.classList.add('calendar-days-selected')
-                selectedLi = li
+            const key = `${year}-${month}-${i}`
+            const events = window.date[key]
+            // console.log(key,events)
+            if(events){
+                li.classList.add('calendar-days-hasEvents')   
             }
-            days.append(li)
+            fragment.append(li)
             liCount += 1
         }
+
+        // 使用事件委托
+        g('.calendar-days').addEventListener('click',(e)=>{
+            const t = e.target
+            if(t.tagName.toLowerCase() === 'li'){
+                console.log(selectedLi)
+                if(selectedLi){
+                    console.log('haha')
+                    selectedLi.classList.remove('calendar-days-selected')
+                }
+                t.classList.add('calendar-days-selected')
+                selectedLi = t
+                const clickKey = `${year}-${month}-${t.textContent}`
+                const clickEvents = window.date[clickKey]
+
+                if(clickEvents){
+                    console.log(clickEvents)
+                    console.log('hi')
+                    //由于不支持 append 多个，且基于不要频繁操作DOM的原则，因此使用 fragment ;
+                    const fragment = document.createDocumentFragment()
+                    clickEvents.map(event => {
+                        const div = document.createElement('div')
+                        div.textContent = event
+                        // div.classList.add('eventItem')
+                        fragment.append(div)
+                        console.log(fragment)
+                    })
+                    g('#events').innerHTML = ''
+                    g('#events').append(fragment)
+                } else{g('#events').innerHTML = "<div>无</div>"}
+            }
+        })
+
         //根据月末星期几，在后铺垫几天
         let i = 月末星期几+1
         for(let j = 1 ; j <= 42-liCount ; j++){
@@ -86,16 +113,10 @@ function render(time){
             const afterDay = new Date(月末 - 0 + 86400*1000*(i-月末星期几))  // -0 转换为数字
             li.textContent = afterDay.getDate()
             li.classList.add('calendar-days-noThisMonth')
-            days.append(li)
-            li.onclick = ()=>{
-                if(selectedLi){
-                    selectedLi.classList.remove('calendar-days-selected')
-                }
-                li.classList.add('calendar-days-selected')
-                selectedLi = li
-            }
+            fragment.append(li)
             i++
         }
+        days.append(fragment)
         // 未实现：点击日历外区域，取消选中；
         // g('.allPage').onclick = (e)=>{
         //     e.stopPropagation()
